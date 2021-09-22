541d37ec352c0ba5830dfbebf071977f8e11d8c6
Jonatan Martens <jonatanmartenstav@gmail.com>
2021-09-22 20:20:44 +0300

    Return id on reservation booking

--- config/sqlConfig.js
+++ config/sqlConfig.js
@@ -9,7 +9,7 @@ const getReservationsQuery = (place_id) =>
     `SELECT * FROM reservations where place_id=${place_id}`;
 
 const createReservationQuery = (place_id, course_id, start_time, end_time) =>
-    `INSERT INTO reservations(place_id, course_id, start_time, end_time) VALUES (${place_id}, ${course_id}, '${start_time}', '${end_time}')`;
+    `INSERT INTO reservations(place_id, course_id, start_time, end_time) VALUES (${place_id}, ${course_id}, '${start_time}', '${end_time}') RETURNING *`;
 
 const getCourseQuery = (course_id) =>
     `SELECT * FROM courses where course_id=${course_id}`;
