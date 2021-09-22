bb19e046085791e7a49e2dabf042fd7848c861bc
Jonatan Martens <jonatanmartenstav@gmail.com>
2021-09-22 19:45:33 +0300

    Add ability to cancel a reservation

--- routers/reservationRouter.js
+++ routers/reservationRouter.js
@@ -2,27 +2,33 @@ const express = require("express");
 const { getReservationsQuery, createReservationQuery } = require("../config/sqlConfig");
 
 const createReservationRouter = (dbConnection) => {
-  const router = express.Router();
+    const router = express.Router();
 
-  router.get("/", async (req, res) => {
-    const place_id = req.query.place_id;
-    const reservations = await dbConnection.QueryData(
-      getReservationsQuery(place_id)
-    );
-    res.json(reservations);
-  });
+    router.get("/", async (req, res) => {
+        const place_id = req.query.place_id;
+        const reservations = await dbConnection.QueryData(
+            getReservationsQuery(place_id)
+        );
+        res.json(reservations);
+    });
 
-  router.post("/", async (req, res) => {
-    const { place_id, course_id, start_time, end_time } = req.body;
-    await dbConnection.QueryData(
-      createReservationQuery(place_id, course_id, start_time, end_time)
-    );
-    res.status(201).send();
-  });
+    router.post("/", async (req, res) => {
+        const { place_id, course_id, start_time, end_time } = req.body;
+        await dbConnection.QueryData(
+            createReservationQuery(place_id, course_id, start_time, end_time)
+        );
+        res.status(201).send();
+    });
 
-  return router;
+    router.delete("/:id", async (req, res) => {
+        const id = req.params.id;
+        await dbConnection.QueryData(`DELETE FROM reservations WHERE reservation_id=${id}`);
+        res.status(204).send();
+    });
+
+    return router;
 };
 
 module.exports = {
-  createReservationRouter,
+    createReservationRouter,
 };
