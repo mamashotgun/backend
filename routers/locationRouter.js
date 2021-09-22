db929eef399fd4ba8af9d0ca0514e3771a4896bd
Jonatan Martens <jonatanmartenstav@gmail.com>
2021-09-22 21:03:23 +0300

    Create route for creating location

--- routers/locationRouter.js
+++ routers/locationRouter.js
@@ -8,6 +8,15 @@ const createLocationRouter = (dbConnection) => {
     res.json(result);
   });
 
+  router.post("/", async (req, res) => {
+    const { name } = req.body;
+    const rows = await dbConnection.QueryData(
+      `INSERT INTO locations(name) VALUES('${name}') RETURNING *`
+    );
+
+    res.status(201).json({ id: rows[0].location_id });
+  });
+
   return router;
 };
 
