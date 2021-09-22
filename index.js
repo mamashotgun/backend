6c71b8e0044412ca69bd8f847d08edd0732cb0bf
Jonatan Martens <jonatanmartenstav@gmail.com>
2021-09-22 21:40:23 +0300

    Fix index.js

--- index.js
+++ index.js
@@ -1,48 +1,27 @@
 const express = require("express");
-const DB = require("./data/DBActions.js");
-const {
-    port,
-    host
-} = require("./config/appConfig");
-const reservationsMiddleware = require("./middleware/reservations/getReservations");
+const DB = require("./data/DBActions");
 const { port, host } = require("./config/appConfig");
 const reservationRouter = require("./routers/reservationRouter");
 const coursesRouter = require("./routers/coursesRouter");
 const categoriesRouter = require("./routers/categoriesRouter");
 const placesRouter = require("./routers/placesRouter");
-
+const locationRouter = require("./routers/locationRouter");
 
 const app = express();
 const dbConnection = new DB();
 
 app.use(express.json());
 app.use(
-    "/reservations",
-    reservationRouter.createReservationRouter(
-        dbConnection,
-    )
+  "/reservations",
+  reservationRouter.createReservationRouter(dbConnection)
 );
 
-app.use(
-    "/courses",
-    coursesRouter.createCourseRouter(dbConnection)
-);
+app.use("/courses", coursesRouter.createCourseRouter(dbConnection));
 
-app.use(
-    "/places",
-    placesRouter.createPlacesRouter(dbConnection)
-);
+app.use("/places", placesRouter.createPlacesRouter(dbConnection));
 
-app.listen(port, host, () => console.log(`listening on port ` + port));
+app.use("/categories", categoriesRouter.createCourseRouter(dbConnection));
 
-app.use(
-  "/categories",
-  categoriesRouter.createCourseRouter(dbConnection)
-);
+app.use("/locations", locationRouter.createLocationRouter(dbConnection));
 
-app.listen(port, host, () => console.log(`listening on port ` + port));
-app.use(
-  "/categories",
-  categoriesRouter.createCourseRouter(dbConnection)
-);
 app.listen(port, host, () => console.log(`listening on port ` + port));
