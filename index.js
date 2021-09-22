8004ebc2cb1b9189ec07f40b8dd7e45b80309d5f
Ido Agassi <ido.agassi@gmail.com>
2021-09-22 19:45:20 +0300

    added logic for LocationsRouter in index.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const DB = require("./data/DBActions.js");
-const { port, host } = require("./config/appConfig");
+const {
+    port,
+    host
+} = require("./config/appConfig");
 const reservationsMiddleware = require("./middleware/reservations/getReservations");
 const reservationRouter = require("./routers/reservationRouter");
 const coursesRouter = require("./routers/coursesRouter");
@@ -11,13 +14,20 @@ const dbConnection = new DB();
 
 app.use(express.json());
 app.use(
-  "/reservations",
-  reservationRouter.createReservationRouter(
-    dbConnection,
-  )
+    "/reservations",
+    reservationRouter.createReservationRouter(
+        dbConnection,
+    )
 );
+
 app.use(
-  "/courses",
-  coursesRouter.createCourseRouter(dbConnection)
+    "/courses",
+    coursesRouter.createCourseRouter(dbConnection)
 );
+
+app.use(
+    "/locations",
+    locationsRouter.createLocationsRouter(dbConnection)
+);
+
 app.listen(port, host, () => console.log(`listening on port ` + port));
