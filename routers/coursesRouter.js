1c62baa3dc93e5ce1185676dab77445d9b00c5b7
Jonatan Martens <jonatanmartenstav@gmail.com>
2021-09-23 03:28:27 +0300

    Return full course object

--- routers/coursesRouter.js
+++ routers/coursesRouter.js
@@ -1,36 +1,26 @@
 const express = require("express");
-const {
-    getCourseQuery,
-    createCourseQuery
-} = require("../config/sqlConfig");
+const { getCourseQuery, createCourseQuery } = require("../config/sqlConfig");
 
 const createCourseRouter = (dbConnection) => {
-    const router = express.Router();
+  const router = express.Router();
 
-    router.get("/", async (req, res) => {
-        const course_id = req.query.course_id;
-        const courses = await dbConnection.QueryData(
-            getCourseQuery(course_id)
-        );
-        res.json(courses)
-    });
+  router.get("/", async (req, res) => {
+    const course_id = req.query.course_id;
+    const courses = await dbConnection.QueryData(getCourseQuery(course_id));
+    res.json(courses);
+  });
 
-    router.post("/", async (req, res) => {
-        const {
-            name,
-            password,
-            is_admin, 
-            display_name
-        } = req.body;
-        await dbConnection.QueryData(
-            createCourseQuery(name, password, is_admin ,display_name)
-        );
-        res.status(201).send();
-    });
+  router.post("/", async (req, res) => {
+    const { name, password, is_admin, display_name } = req.body;
+    const rows = await dbConnection.QueryData(
+      createCourseQuery(name, password, is_admin, display_name)
+    );
+    res.status(201).json(rows[0]);
+  });
 
-    return router;
+  return router;
 };
 
 module.exports = {
-    createCourseRouter,
+  createCourseRouter,
 };
